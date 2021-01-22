79d1d0edcfed24b6df20639b84ff4a3e1a7d79c0
ToniF03 <44695452+ToniF03@users.noreply.github.com>
2021-01-22 22:29:15 +0100

    Update ModernFolderBrowserDialog.cs

--- ModernFolderBrowserDialog.cs
+++ ModernFolderBrowserDialog.cs
@@ -1,21 +1,39 @@
 using Microsoft.WindowsAPICodePack.Dialogs;
+using System.ComponentModel;
 
     class ModernFolderBrowserDialog : DialogControl
     {
         CommonOpenFileDialog dialog = new CommonOpenFileDialog();
 
-        public void ShowDialog()
+        [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
+        public DialogResult ShowDialog()
         {
             dialog.EnsurePathExists = true;
             dialog.EnsureFileExists = false;
             dialog.AllowNonFileSystemItems = false;
             dialog.IsFolderPicker = true;
-            dialog.ShowDialog();
-            string s = dialog.FileName;
+            CommonFileDialogResult dr = dialog.ShowDialog();
+
+            switch (dr) {
+                case CommonFileDialogResult.Ok:
+                    return DialogResult.OK;
+                case CommonFileDialogResult.None:
+                    return DialogResult.None;
+                case CommonFileDialogResult.Cancel:
+                    return DialogResult.Cancel;
+            }
+            return DialogResult.None;
         }
 
         public string FolderPath
         {
             get { return dialog.FileName; }
         }
+    }
+
+    public enum DialogResult
+    {
+        OK,
+        None,
+        Cancel
     }
